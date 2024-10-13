c795ca2228cc24cb5f52a4002b2ab2ef1668479c
Samir Jose Giil <144859085+SamirJoseGil@users.noreply.github.com>
2024-10-13 16:18:36 -0500

    Update NarratorTests.cs

--- katio_net.Test/NarratorTests.cs
+++ katio_net.Test/NarratorTests.cs
@@ -7,6 +7,7 @@ using katio.Business.Services;
 using System.Linq.Expressions;
 using katio.Data.Dto;
 using System.Net;
+using System.Xml.Linq;
 
 
 namespace katio.Test;
@@ -43,7 +44,8 @@ public class NarratorTests
     }
 
 
-    
+    #region Test Methods Simple
+
     // Test for getting all narrators
     [TestMethod]
     public async Task GetAllNarrators() 
@@ -56,6 +58,86 @@ public class NarratorTests
 
         // Assert
         Assert.AreEqual(2, result.ResponseElements.Count());
+    }
+
+
+    // Test for creating a narrator
+    [TestMethod]
+    public async Task CreateNarrator()
+    {
+        // Arrange
+        var narrator = new Narrator
+        {
+            Name = "Maria Camila",
+            LastName = "Gil Rojas",
+            Genre = "Ficcion"
+        };
+        _narratorRepository.GetAllAsync(Arg.Any<Expression<Func<Narrator, bool>>>()).Returns(new List<Narrator>());
+        _narratorRepository.AddAsync(narrator).Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _narratorService.CreateNarrator(narrator);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.OK_200, result.Message);
+        Assert.AreEqual(narrator.Name, result.ResponseElements.First().Name);
+        Assert.AreEqual(narrator.LastName, result.ResponseElements.First().LastName);
+        Assert.AreEqual(narrator.Genre, result.ResponseElements.First().Genre);
+    }
+
+
+    // Test for updating a narrator
+    [TestMethod]
+    public async Task UpdateNarrator()
+    {
+        // Arrange
+        var narratorToUpdate = _narrators.First();
+        _narratorRepository.FindAsync(narratorToUpdate.Id).Returns(narratorToUpdate);
+
+        var updatedNarrator = new Narrator
+        {
+            Id = narratorToUpdate.Id,
+            Name = "Maria Camila",
+            LastName = "Gil Rojas",
+            Genre = "Ficcion"
+        };
+        _narratorRepository.FindAsync(updatedNarrator.Id).Returns(updatedNarrator);
+
+        _narratorRepository.Update(updatedNarrator).Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _narratorService.UpdateNarrator(updatedNarrator);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.OK_200, result.Message);
+        Assert.AreEqual(updatedNarrator.Name, result.ResponseElements.First().Name);
+        Assert.AreEqual(updatedNarrator.LastName, result.ResponseElements.First().LastName);
+        Assert.AreEqual(updatedNarrator.Genre, result.ResponseElements.First().Genre);
+    }
+
+
+    // Test for deleting a narrator
+    [TestMethod]
+    public async Task DeleteNarrator()
+    {
+        // Arrange
+        var narratorToDelete = _narrators.First();
+        _narratorRepository.FindAsync(narratorToDelete.Id).Returns(narratorToDelete);
+
+        _narratorRepository.Delete(narratorToDelete).Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _narratorService.DeleteNarrator(narratorToDelete.Id);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.OK_200, result.Message);
+        await _narratorRepository.Received().Delete(narratorToDelete);
     }
 
 
@@ -72,6 +154,8 @@ public class NarratorTests
 
         // Assert
         Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.OK_200, result.Message);
         Assert.AreEqual(narrator.Name, result.ResponseElements.First().Name);
     }
 
@@ -89,26 +173,165 @@ public class NarratorTests
 
         // Assert
         Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.OK_200, result.Message);
         Assert.AreEqual(narrator.Name, result.ResponseElements.First().Name);
     }
 
 
-    // Repository Exeptions
-    // Test for getting a narrator by Id with repository exceptions
+    // Test for getting a narrator by Last Name
     [TestMethod]
-    public async Task GetNarratorByIdRepositoryException()
+    public async Task GetNarratorsByLastName()
     {
         // Arrange
-        var narratorId = 1;
-        _narratorRepository.FindAsync(narratorId).Returns<Task<Narrator>>( x => { throw new Exception("Repository error"); });
+        var narrator = _narrators.First();
+        _narratorRepository.GetAllAsync(Arg.Any<Expression<Func<Narrator, bool>>>()).Returns(new List<Narrator> { narrator });
 
         // Act
-        var result = await _narratorService.GetNarratorById(narratorId);
+        var result = await _narratorService.GetNarratorsByLastName(narrator.LastName);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.OK_200, result.Message);
+        Assert.AreEqual(narrator.LastName, result.ResponseElements.First().LastName);
+    }
+
+
+    // Test for getting a narrator by Genre
+    [TestMethod]
+    public async Task GetNarratorsByGenre()
+    {
+        // Arrange
+        var narrator = _narrators.First();
+        _narratorRepository.GetAllAsync(Arg.Any<Expression<Func<Narrator, bool>>>()).Returns(new List<Narrator> { narrator });
+
+        // Act
+        var result = await _narratorService.GetNarratorsByGenre(narrator.Genre);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.OK_200, result.Message);
+        Assert.AreEqual(narrator.Genre, result.ResponseElements.First().Genre);
+    }
+    #endregion
+
+    #region Repository Exceptions
+
+    // Test for getting all narrators with repository exceptions
+    [TestMethod]
+    public async Task GetAllNarratorsRepositoryException()
+    {
+        // Arrange
+        _narratorRepository.When(x => x.GetAllAsync()).Do(x => throw new Exception("Repository error"));
+
+        // Act
+        var result = await _narratorService.Index();
 
         // Assert
         Assert.IsNotNull(result);
         Assert.AreEqual(HttpStatusCode.InternalServerError, result.StatusCode);
         Assert.AreEqual(BaseMessageStatus.INTERNAL_SERVER_ERROR_500, result.Message);
+        Assert.AreEqual("500 Internal Server Error | Repository error", result.Message);
+    }
+
+
+    // Test for creating a narrator with repository exceptions
+    [TestMethod]
+    public async Task CreateNarratorRepositoryException()
+    {
+        // Arrange
+        var narrator = new Narrator
+        {
+            Name = "Maria Camila",
+            LastName = "Gil Rojas",
+            Genre = "Ficcion"
+        };
+
+        _narratorRepository.GetAllAsync(Arg.Any<Expression<Func<Narrator, bool>>>()).Returns(new List<Narrator>());
+
+        _narratorRepository.When(x => x.AddAsync(Arg.Any<Narrator>())).Do(x => throw new Exception("Repository error"));
+
+        // Act
+        var result = await _narratorService.CreateNarrator(narrator);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.InternalServerError, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.INTERNAL_SERVER_ERROR_500, result.Message);
+        Assert.AreEqual("500 Internal Server Error | Repository error", result.Message);
+    }
+
+
+    // Test for updating a narrator with repository exceptions
+    [TestMethod]
+    public async Task UpdateNarratorRepositoryException()
+    {
+        // Arrange
+        var narratorToUpdate = _narrators.First();
+        _narratorRepository.FindAsync(narratorToUpdate.Id).Returns(narratorToUpdate);
+
+        var updatedNarrator = new Narrator
+        {
+            Id = narratorToUpdate.Id,
+            Name = "Maria Camila",
+            LastName = "Gil Rojas",
+            Genre = "Ficcion"
+        };
+
+        _narratorRepository.FindAsync(updatedNarrator.Id).Returns(updatedNarrator);
+
+        _narratorRepository.When(x => x.Update(updatedNarrator)).Do(x => throw new Exception("Repository error"));
+
+        // Act
+        var result = await _narratorService.UpdateNarrator(updatedNarrator);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.InternalServerError, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.INTERNAL_SERVER_ERROR_500, result.Message);
+        Assert.AreEqual("500 Internal Server Error | Repository error", result.Message);
+    }
+
+
+    // Test for deleting a narrator with repository exceptions
+    [TestMethod]
+    public async Task DeleteNarratorRepositoryException()
+    {
+        // Arrange
+        var narratorToDelete = _narrators.First();
+        _narratorRepository.FindAsync(narratorToDelete.Id).Returns(narratorToDelete);
+
+        _narratorRepository.When(x => x.Delete(narratorToDelete)).Do(x => throw new Exception("Repository error"));
+
+        // Act
+        var result = await _narratorService.DeleteNarrator(narratorToDelete.Id);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.InternalServerError, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.INTERNAL_SERVER_ERROR_500, result.Message);
+        Assert.AreEqual("500 Internal Server Error | Repository error", result.Message);
+    }
+
+
+    // Test for getting a narrator by Id with repository exceptions
+    [TestMethod]
+    public async Task GetNarratorByIdRepositoryException()
+    {
+        // Arrange
+        var narrator = _narrators.First();
+        _narratorRepository.When(x => x.FindAsync(narrator.Id)).Do(x => throw new Exception("Repository error"));
+
+        // Act
+        var result = await _narratorService.GetNarratorById(narrator.Id);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.InternalServerError, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.INTERNAL_SERVER_ERROR_500, result.Message);
+        Assert.AreEqual("500 Internal Server Error | Repository error", result.Message);
     }
 
 
@@ -118,7 +341,7 @@ public class NarratorTests
     {
         // Arrange
         var narrator = _narrators.First();
-        _narratorRepository.GetAllAsync(Arg.Any<Expression<Func<Narrator, bool>>>()).Returns<Task<List<Narrator>>>(x => { throw new Exception("Repository error"); });
+        _narratorRepository.When(x => x.GetAllAsync(Arg.Any<Expression<Func<Narrator, bool>>>())).Do(x => throw new Exception("Repository error"));
 
         // Act
         var result = await _narratorService.GetNarratorsByName(narrator.Name);
@@ -127,8 +350,46 @@ public class NarratorTests
         Assert.IsNotNull(result);
         Assert.AreEqual(HttpStatusCode.InternalServerError, result.StatusCode);
         Assert.AreEqual(BaseMessageStatus.INTERNAL_SERVER_ERROR_500, result.Message);
+        Assert.AreEqual("500 Internal Server Error | Repository error", result.Message);
     }
 
 
+    // Test for getting a narrator by last name with repository exceptions
+    [TestMethod]
+    public async Task GetNarratorsByLastNameRepositoryException()
+    {
+        // Arrange
+        var narrator = _narrators.First();
+        _narratorRepository.When(x => x.GetAllAsync(Arg.Any<Expression<Func<Narrator, bool>>>())).Do(x => throw new Exception("Repository error"));
 
+        // Act
+        var result = await _narratorService.GetNarratorsByLastName(narrator.LastName);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.InternalServerError, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.INTERNAL_SERVER_ERROR_500, result.Message);
+        Assert.AreEqual("500 Internal Server Error | Repository error", result.Message);
+    }
+
+
+    // Test for getting a narrator by genre with repository exceptions
+    [TestMethod]
+    public async Task GetNarratorsByGenreRepositoryException()
+    {
+        // Arrange
+        var narrator = _narrators.First();
+        _narratorRepository.When(x => x.GetAllAsync(Arg.Any<Expression<Func<Narrator, bool>>>())).Do(x => throw new Exception("Repository error"));
+
+        // Act
+        var result = await _narratorService.GetNarratorsByGenre(narrator.Genre);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.InternalServerError, result.StatusCode);
+        Assert.AreEqual(BaseMessageStatus.INTERNAL_SERVER_ERROR_500, result.Message);
+        Assert.AreEqual("500 Internal Server Error | Repository error", result.Message);
+    }
+
+    #endregion
 }
